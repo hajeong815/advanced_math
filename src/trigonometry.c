5c4c006209a4527bbd6c61d9131828f10f234b84
Hajeong Jeon <62226517+hajeong815@users.noreply.github.com>
2024-11-23 04:59:22 +0100

    Create trigonometry.c

---
+++ src/trigonometry.c
@@ -0,0 +1,13 @@
+#include "math/advanced/trigonometry.h"
+
+double sine(double angle) {
+    return sin(angle);
+}
+
+double cosine(double angle) {
+    return cos(angle);
+}
+
+double tangent(double angle) {
+    return tan(angle);
+}
