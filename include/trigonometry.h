92dd404ad0e9bdfb62c134a80f9fd5b05119b96c
Hajeong Jeon <62226517+hajeong815@users.noreply.github.com>
2024-11-23 04:57:30 +0100

    Create trigonometry.h

---
+++ include/trigonometry.h
@@ -0,0 +1,10 @@
+#ifndef TRIGONOMETRY_H
+#define TRIGONOMETRY_H
+
+#include <math.h>
+
+double sine(double angle);
+double cosine(double angle);
+double tangent(double angle);
+
+#endif // TRIGONOMETRY_H
